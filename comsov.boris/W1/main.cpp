e38f54fab4b42148a82a56f1f35a3e30edf688f9
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-02-12 18:19:55 +0300

    add expandLengths and add 'size' trait

--- comsov.boris/W1/main.cpp
+++ comsov.boris/W1/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <cstring>
+
+void expandLengths(size_t **lengths, size_t size) {
+  size_t newSize = (size == 0) ? 2 : size * 2;
+  size_t *newLengths = new size_t [newSize];
+  for (size_t i = 0; i < size; ++i) {
+    newLengths[i] = (*lengths)[i];
+  }
+  delete[] *lengths;
+  *lengths = newLengths;
+}
 
 void expand(int*** arr, size_t& oldSize) {
-
   size_t newSize = (oldSize == 0) ? 2 : oldSize * 2;
   int** newArr = new int* [newSize];
   for (size_t i = 0; i < oldSize; ++i) {
-    newArr[i] = *arr[i];
+    newArr[i] = (*arr)[i];
   }
   delete[] *arr;
   *arr = newArr;
@@ -14,22 +24,33 @@ void expand(int*** arr, size_t& oldSize) {
 
 int main(int argc, char** argv)
 {
-
   size_t s = 0;
-  int** arrs = nullptr;
+  int **arrs = nullptr;
   size_t cap = 0;
+  size_t *lengths = nullptr;
   int n = 0;
 
   while (std::cin >> n) {
     int* arr = new int[n];
-
     for (size_t i = 0; i < n; ++i) {
       std::cin >> arr[i];
     }
     if (cap <= s) {
+      size_t capCoppy = cap;
       expand(&arrs, cap);
+      expandLengths(&lengths, capCoppy);
     }
     arrs[s] = arr;
+    lengths[s] = n;
     s++;
+  }
+  if (std::strcmp(argv[1], "size") == 0) {
+    size_t totalSize = 0;
+    for (size_t i = 0; i < cap; ++i) {
+      for (size_t j = 0; j < lengths[i]; ++j) {
+        ++totalSize;
+      }
+    }
+    std::cout << totalSize << "\n";
   }
 }
